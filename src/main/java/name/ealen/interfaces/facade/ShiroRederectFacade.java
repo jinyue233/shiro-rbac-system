f9f715ef6a38e99e9eb870dd498423e90761c387
yuanmabiji <13570990660@163.com>
2022-03-06 10:57:45 +0800

    添加覆盖和删除cookie的方法测试

--- src/main/java/name/ealen/interfaces/facade/ShiroRederectFacade.java
+++ src/main/java/name/ealen/interfaces/facade/ShiroRederectFacade.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @RestController
 public class ShiroRederectFacade {
+    int i;
 
     // 【结论1】重定向添加的cookie设置进主机domain(若Host为shiro.com，则返回给浏览器后的cookie属于shiro.com这个domain)
     @GetMapping(value = "/open/api/redirect")
@@ -48,5 +49,22 @@ public class ShiroRederectFacade {
         cookie.setPath("/");
         response.addCookie(cookie);
         WebUtils.issueRedirect(request, response, "http://cas.com:8088/login");
+    }
+
+    // 【结论3】重定向添加的cookie若设置domain为其他domain（主机Host为shiro.com，设置其他domain为cas.com，并设置path为"/"，
+    // 此时cookie可以返回给浏览器，但会被浏览器bolcked，提示blocked信息，因此浏览器访问访问cas.com不会带着此cookie过去）
+    @GetMapping(value = "/open/api/testCookieOveride")
+    public void testCookieOveride(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        Cookie cookie = new Cookie("name", "jinyue" + i++);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+    }
+
+    @GetMapping(value = "/open/api/testCookieDelete")
+    public void testCookieDelete(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        Cookie cookie = new Cookie("name", "jinyue" + i++);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 }
